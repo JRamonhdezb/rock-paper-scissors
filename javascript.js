2339a8d452c7648fc0f06a9b2836eaaa23b531cc
JRamonhdezb <141176160+JRamonhdezb@users.noreply.github.com>
2024-08-26 11:25:09 -0700

    Add getHumanChoice function
    The function will prompt the user an input value
    between rock, paper and scissor and stores it in
    a new variable

--- javascript.js
+++ javascript.js
@@ -18,3 +18,28 @@ function getsComputerChoice() {
         return "scissor";
     }
 }
+
+// Function of the human choice, it will take the user choice and return it
+// Prompt the user a value between rock, paper or scissor
+// Store the value in humanChoice variable
+// Conditional statement to assign and return the choice of the user
+
+function getHumanChoice() {
+    let humanChoice = prompt("What is your choice: rock, paper or scissor", "rock");
+    humanChoice = humanChoice.toLowerCase();
+    if (humanChoice === "rock") {
+        console.log(humanChoice); 
+        return "rock";
+    } else if (humanChoice === "paper") {
+        console.log(humanChoice);
+        return "paper";
+    } else if (humanChoice === "scissor") {
+        console.log(humanChoice);
+        return "scissor";
+    } else {
+        console.log("error");
+    }
+
+}
+
+getHumanChoice();
